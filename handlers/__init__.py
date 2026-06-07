5b1a3f912fefe4f4310c4aa0d20ae1f753d55383
moha100h <mmlu.tel@gmail.com>
2026-06-07 13:21:23 +0330

    feat: register settings router

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Router
 from .cmd import router as cmd_r
 from .search import router as search_r
+from .settings import router as settings_r
 
 router = Router()
+router.include_router(settings_r)
 router.include_router(cmd_r)
 router.include_router(search_r)
