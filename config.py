0a57e32b608b3a059e79a912f1b12943de7481d3
moha100h <mmlu.tel@gmail.com>
2026-06-07 13:18:59 +0330

    feat: add NESHAN_API_KEY config

--- config.py
+++ config.py
@@ -2,11 +2,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKEN   = os.getenv('BOT_TOKEN', '')
-ADMIN_ID    = int(os.getenv('ADMIN_ID', '0'))
-MAX_RESULTS = int(os.getenv('MAX_RESULTS', '200'))
-DB_PATH     = os.getenv('DB_PATH', './data/kassb.db')
-EXPORT_DIR  = os.getenv('EXPORT_DIR', './data/exports')
+BOT_TOKEN      = os.getenv('BOT_TOKEN', '')
+ADMIN_ID       = int(os.getenv('ADMIN_ID', '0'))
+MAX_RESULTS    = int(os.getenv('MAX_RESULTS', '200'))
+DB_PATH        = os.getenv('DB_PATH', './data/kassb.db')
+EXPORT_DIR     = os.getenv('EXPORT_DIR', './data/exports')
+NESHAN_API_KEY = os.getenv('NESHAN_API_KEY', '')
 
 if not BOT_TOKEN:
     raise ValueError('BOT_TOKEN is not set in .env')
